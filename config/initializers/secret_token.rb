5681da57524ff02d7f3e95ac0e64a08557f54f17
Adebamigbe <adebamigbe.fasanmade@gmail.com>
2013-10-28 16:44:26 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GAplanner::Application.config.secret_token = 'd5b7c2fa2d0acb38e5304a3745812bc01c0b8344ed5dc88ac454c297d5475478ff4433ff1ad518a753f8cc1d8cc6a2f99627dce70bde2315c4870902a08358d3'
